3e40bcb68c4215f9228a3eedbca5978b172cf743
Dom Amato <dom@hologram.io>
2020-09-15 15:13:24 -0500

    start work on proton native version

--- runsigntool.bat
+++
@@ -1,3 +0,0 @@
-rem This is all very specific to one type of setup
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" || exit /b 1
-signtool sign /a /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 dist\spacebridge.exe || exit /b 1
